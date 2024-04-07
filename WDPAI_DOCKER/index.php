4ef9341fd67dd595ef7351230bd61adb14be0f29
Grelory <57916931+Grelory@users.noreply.github.com>
2024-04-07 12:35:23 +0200

    Lab3 Add more css styles

--- WDPAI_DOCKER/index.php
+++ WDPAI_DOCKER/index.php
@@ -9,4 +9,20 @@ $path = parse_url($path, PHP_URL_PATH);
 $action = explode("/", $path)[0];
 $action = $action == null ? 'login': $action;
 
+switch($action) {
+    case 'dashboard':
+        $title = "PROJECTS";
+        $projects = [];
+
+        $controller->render($action)
+        break;
+    case 'login':
+        $title = "LOGIN";
+
+        $controller->render($action)
+        break;
+    default:
+        $controller->render($action);
+}
+
 $controller->render($action);
---
+++ WDPAI_DOCKER/public/scripts/mobile-nav.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector("#menu");
+const menu = document.querySelector(".sidebar");
+
+menu.addEventListener("click", () => {
+    menu.style.diplay = "block";
+})
