40a9d6266c11c27d6376176cad8ebeeb4f370e5a
Grelory <gmpwm99@gmail.com>
2024-08-14 18:09:33 +0200

    QB-3 Implement basic navigation for user

---
+++ WDPAI_DOCKER/public/scripts/user/navigation-bar.js
@@ -0,0 +1,42 @@
+
+const homePath = '/public/views/user/'
+
+const icons = [
+
+    { 'letter': 'H', 'fullName': 'Dashboard', 'handler': () => goto('dashboard')},
+    { 'letter': 'F', 'fullName': 'Favourites', 'handler': () => goto('favourites')},
+    { 'letter': 'B', 'fullName': 'Buy', 'handler': () => goto('buy')},
+    { 'letter': 'A', 'fullName': 'Available', 'handler': () => goto('available')},
+    { 'letter': 'E', 'fullName': 'Expired', 'handler': () => goto('expired')},
+    { 'letter': 'Q', 'fullName': 'Logout', 'handler': () => goto('logout')}
+]
+
+function goto(destination) {
+    console.log('Requested GOTO: ' + destination)
+    window.location = homePath + destination + '.html'
+}
+
+function navbarElements() {
+    
+    return icons.map(icon => {
+        const element = document.createElement('p')
+        element.appendChild(document.createTextNode(icon.letter))
+        element.classList.add('icon')
+        element.addEventListener('click', icon.handler)
+        return element
+    })
+
+}
+
+function renderNavigationBar() {
+    const body = document.querySelector('body')
+
+    const div = document.createElement('div')
+    div.setAttribute('id', 'navbar')
+
+    navbarElements().forEach(element => div.appendChild(element))
+
+    body.appendChild(div)
+}
+
+renderNavigationBar()
