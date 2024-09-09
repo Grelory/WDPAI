97f96b1f159f68b822c20633b3db84e387727f5e
Grelory <gmpwm99@gmail.com>
2024-09-09 23:39:43 +0200

    QB-39 Implement new navigation bar

---
+++ WDPAI_DOCKER/public/scripts/admin/buttons.js
@@ -0,0 +1,17 @@
+const options = [
+    { name: 'dashboard', path: '/admin/dashboard'},
+    { name: 'tickets', path: '/admin/tickets'},
+    { name: 'locations', path: '/admin/locations'},
+    { name: 'providers', path: '/admin/providers'},
+    { name: 'transport', path: '/admin/transport'},
+    { name: 'types', path: '/admin/types'},
+    { name: 'logout', path: '/auth/logout'}
+]
+
+options.forEach((option) => {
+    document.querySelectorAll('div.go-to-' + option['name']).forEach(button => {
+        button.addEventListener('click', () => {
+            window.location = option['path'];
+        })
+    });
+})
